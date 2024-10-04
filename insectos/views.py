455f2c31ad445f9a02d3ad207d0cdfb20ff7f76d
Martin <martin.munoz5736@alumnos.udg.mx>
2024-10-03 22:07:21 -0600

    Se modifico el archivo views.py de la aplicacion insectos donde agregamos la tabla Insectos para mostrarla en el archivo insectos.html

--- insectos/views.py
+++ insectos/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Insectos
 
 # Create your views here.
 
 def insectos(request):
 
+    insectos = Insectos.objects.all()
     return render(request, "insectos/insectos.html", {"insectos":insectos})
