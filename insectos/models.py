4ffad78c4ae15709ac5cea0a500bf8b0ae3461ae
Martin <martin.munoz5736@alumnos.udg.mx>
2024-10-03 22:05:37 -0600

    Se modifico el archivo models.py de la aplicacion insectos donde se crearon las tablas Estado e Insectos cada una con sus respectivos campos

--- insectos/models.py
+++ insectos/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Estado(models.Model):
+    nombreEstado=models.CharField(max_length=50, verbose_name='Nombre')
+
+    class Meta:
+        verbose_name = "estado"
+        verbose_name_plural = "estados"
+    
+    def __str__(self):
+        return self.nombreEstado
+
+class Insectos(models.Model):
+    nombreInsecto = models.CharField(max_length=30, verbose_name='Nombre')
+    descripcion = models.CharField(max_length=100, verbose_name='Descripcion')
+    imagen = models.ImageField(upload_to='insectos')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    categoriaInsectos=models.ManyToManyField(Estado)
+
+    class Meta:
+        verbose_name = 'insecto'
+        verbose_name_plural = 'insectos'
+    
+    def __str__(self):
+        return self.nombreInsecto
