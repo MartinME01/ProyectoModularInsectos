db7086f791b6c79029da1c2bc9f33574e21ea1c4
Martin <martin.munoz5736@alumnos.udg.mx>
2024-10-03 21:49:31 -0600

    Se agrego la vista para el login en views.py

--- login/views.py
+++ login/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .forms import FormularioLogin
 
 # Create your views here.
+
+def login(request):
+
+    if request.method == "POST":
+
+        formularioLogin = FormularioLogin(request.POST)
+
+        if formularioLogin.is_valid:
+
+            #informacionLogin = formularioLogin.cleaned_data
+
+            return render(request, "login/gracias.html")
+
+    else:
+
+        formularioLogin = FormularioLogin()
+
+
+    return render(request, "login/login.html", {"form":formularioLogin})
