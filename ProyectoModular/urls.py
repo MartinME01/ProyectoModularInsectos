350f65d6e0abd88eb49e20d988497d9f6f0162c6
Martin <martin.munoz5736@alumnos.udg.mx>
2024-10-03 21:57:34 -0600

    Se modifico el archivo urls.py del proyecto principal para agregar la url de la aplicacion login

--- ProyectoModular/urls.py
+++ ProyectoModular/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('inicio/', include('inicio.urls'))
+    path('', include('inicio.urls')),
+    path('login/', include('login.urls')),
+    
 ]
