60f936581dbd2bee87e69b7ff6c762eda2d5259c
Martin <martin.munoz5736@alumnos.udg.mx>
2024-10-03 22:08:41 -0600

    Se modifico el archivo admin.py de la aplicacion insectos para mostrar las tablas creadas en el models.py en el panel de administracion

--- insectos/admin.py
+++ insectos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Insectos, Estado
 
 # Register your models here.
+
+class InsectosAdmin(admin.ModelAdmin):
+    readonly_fields=('created', 'updated')
+
+
+admin.site.register(Insectos, InsectosAdmin)
+admin.site.register(Estado)
