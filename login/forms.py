e389a30126ad8a6dd11bb32ed1bd6e7985932436
Martin <martin.munoz5736@alumnos.udg.mx>
2024-10-03 21:50:52 -0600

    Se creo el archivo register.html y se modifico el forms.py para crear el formulario del registro

--- login/forms.py
+++ login/forms.py
@@ -4,3 +4,9 @@ class FormularioLogin(forms.Form):
 
     usuario = forms.CharField(label="Nombre de usuario", max_length=100)
     contrasenia = forms.CharField(label="Introduzca contraseña", widget=forms.PasswordInput)
+
+class FormularioRegister(forms.Form):
+
+    usuario = forms.CharField(label="Nombre del usuario", max_length=100)
+    email = forms.EmailField(label="Ingresa email")
+    contrasenia = forms.CharField(label="Ingresa contraseña", widget=forms.PasswordInput)
