549f53b6cdee33c96fea4bee32c33ac8e78d8e35
Martin <martin.munoz5736@alumnos.udg.mx>
2024-10-04 19:33:13 -0600

    Se agrego la carpeta media al proyecto principal, tambien se odifico el archivo urls.py para que pueda rescatar la url de la imagen

--- ProyectoModular/urls.py
+++ ProyectoModular/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('inicio.urls')),
     path('login/', include('login.urls')),
-    path('insectos/', include('insectos.urls'))
-    
+    path('insectos/', include('insectos.urls')),
 ]
+
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
